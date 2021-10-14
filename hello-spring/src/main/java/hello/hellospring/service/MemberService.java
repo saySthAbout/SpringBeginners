c22c3c6273ff50f724cff4a084c13281b9c6bc3a
saySthAbout <saysthabout@gmail.com>
2021-10-14 17:43:31 +0900

    JPA(Java Persistance API)

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,17 +5,17 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
-
 
     private final MemberRepository memberRepository;
 
     // Dependency Injection
-
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
